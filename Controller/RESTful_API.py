d3a05b5e9326f5c49e9cb0842f119e793d398717
rhongho <42688206+rhongho@users.noreply.github.com>
2020-09-28 06:53:47 -0400

    Add files via upload

---
+++ Controller/RESTful_API.py
@@ -0,0 +1,186 @@
+import json
+from bson import json_util
+import logging
+import uuid
+from wsgiref import simple_server
+import falcon
+import requests
+from pymongo import MongoClient
+myclient = MongoClient('localhost', 27017)
+mydb = myclient.RflowCollector
+FlowCol = mydb.FlowRecord
+
+class StorageEngine(object):
+
+    def get_stat(self, rflow_object):
+        return list(FlowCol.find(rflow_object))
+
+    def add_rule(self, rflow_object):
+        rule['id'] = rflow_object
+        return rule
+
+class StorageError(Exception):
+
+    @staticmethod
+    def handle(ex, req, resp, params):
+        description = ('Sorry, couldn\'t write your thing to the '
+                       'database. It worked on my box.')
+
+        raise falcon.HTTPError(falcon.HTTP_725,
+                               'Database Error',
+                               description)
+
+class AuthMiddleware(object):
+
+    def process_request(self, req, resp):
+        token = req.get_header('Authorization')
+        account_id = req.get_header('Account-ID')
+
+        challenges = ['Token type="Fernet"']
+        if token is None:
+            description = ('Please provide an auth token '
+                           'as part of the request.')
+                           
+            raise falcon.HTTPUnauthorized('Auth token required',
+                                          description,
+                                          challenges,
+                                          href='http://docs.example.com/auth')
+
+        if not self._token_is_valid(token, account_id):
+            description = ('The provided auth token is not valid. '
+                           'Please request a new token and try again.')
+
+            raise falcon.HTTPUnauthorized('Authentication required',
+                                          description,
+                                          challenges,
+                                          href='http://docs.example.com/auth')
+
+    def _token_is_valid(self, token, account_id):
+        return True  # All are valid...
+
+
+class RequireJSON(object):
+
+    def process_request(self, req, resp):
+        if not req.client_accepts_json:
+            raise falcon.HTTPNotAcceptable(
+                'This API only supports responses encoded as JSON.',
+                href='http://docs.examples.com/api/json')
+
+        if req.method in ('POST', 'PUT'):
+            if 'application/json' not in req.content_type:
+                raise falcon.HTTPUnsupportedMediaType(
+                    'This API only supports requests encoded as JSON.',
+                    href='http://docs.examples.com/api/json')
+
+
+class JSONTranslator(object):
+    def process_request(self, req, resp):
+        # req.stream corresponds to the WSGI wsgi.input environ variable,
+        # and allows you to read bytes from the request body.
+        #
+        # See also: PEP 3333
+        if req.content_length in (None, 0):
+            # Nothing to do
+            return
+
+        body = req.bounded_stream.read()
+
+        if not body:
+            raise falcon.HTTPBadRequest('Empty request body',
+                                        'A valid JSON document is required.')
+
+        try:
+            req.context.doc = json.loads(body.decode('utf-8'))
+
+        except (ValueError, UnicodeDecodeError):
+            raise falcon.HTTPError(falcon.HTTP_753,
+                                   'Malformed JSON',
+                                   'Could not decode the request body. The '
+                                   'JSON was incorrect or not encoded as '
+                                   'UTF-8.')
+
+    def process_response(self, req, resp, resource, req_succeeded):
+        if not hasattr(resp.context, 'result'):
+            return
+                   
+        resp.body = json.dumps(resp.context.result, default=json_util.default)
+
+def max_body(limit):
+
+    def hook(req, resp, resource, params):
+        length = req.content_length
+        if length is not None and length > limit:
+            msg = ('The size of the request is too large. The body must not '
+                   'exceed ' + str(limit) + ' bytes in length.')
+
+            raise falcon.HTTPPayloadTooLarge(
+                'Request body is too large', msg)
+
+    return hook
+
+
+class ThingsResource(object):
+
+    def __init__(self, db):
+        self.db = db
+        self.logger = logging.getLogger('RflowCollector.' + __name__)
+
+    def on_get(self, req, resp, user_id):
+        rflow_object = req.context.doc
+        marker = rflow_object
+	
+        if rflow_object['type'] == "Statistics":
+            try:
+            	del rflow_object['type']            
+            	result = self.db.get_stat(rflow_object);	
+            except Exception as ex:
+            	self.logger.error(ex)
+
+            	description = ('Aliens have attacked our base! We will '
+                           'be back as soon as we fight them off. '
+                           'We appreciate your patience.')
+
+            	raise falcon.HTTPServiceUnavailable(
+                'Service Outage',
+                description,
+                30)
+
+        resp.context.result = result
+        resp.set_header('Powered-By', 'RFlow_Collector')
+        resp.status = falcon.HTTP_200
+
+    @falcon.before(max_body(64 * 1024))
+    def on_post(self, req, resp, user_id):
+        rflow_object = resp.context.doc
+        try:
+            doc = req.context.doc
+        except AttributeError:
+            raise falcon.HTTPBadRequest(
+                'Missing Rflow_object',
+                'A Rflow_object must be submitted in the request body.')
+
+        rules = self.db.add_rule(doc)
+
+        resp.status = falcon.HTTP_201
+        resp.location = '/%s/rflow_rollector/%s' % (user_id, rules['id'])
+
+
+# Configure your WSGI server to load "things.app" (app is a WSGI callable)
+app = falcon.API(middleware=[
+    AuthMiddleware(),
+    RequireJSON(),
+    JSONTranslator(),
+])
+
+db = StorageEngine()
+Rflow_Collector = ThingsResource(db)
+app.add_route('/{user_id}/rflow_collector', Rflow_Collector)
+
+
+app.add_error_handler(StorageError, StorageError.handle)
+
+
+if __name__ == '__main__':
+    httpd = simple_server.make_server('127.0.0.1', 8000, app)
+    httpd.serve_forever()
---
+++ Controller/RFlow_Collector.py
@@ -0,0 +1,100 @@
+import threading 
+import socket 
+import json
+from pymongo import MongoClient
+from _thread import *
+  
+print_lock = threading.Lock() 
+
+myclient = MongoClient('localhost', 27017)
+mydb = myclient["RflowCollector"]
+FlowCol = mydb["FlowRecord"]
+
+# thread function 
+def threaded(c): 
+    while True: 
+  
+        # data received from client 
+        data = c.recv(4096)
+        if not data: 
+            print('Bye') 
+            
+            # lock released on exit 
+            print_lock.release() 
+            break
+  	
+        json_data = json.loads(data)
+        ID = json_data["ID"]
+        Layer = json_data["Layer"]
+        for x in range(len(json_data["Flow_Record"])):
+        	for y in json_data["Flow_Record"]:
+            		if json_data["Layer"] == 2:
+            			dict={"ID":ID, "Layer":Layer, "Hash": y[0], "src_MAC": y[1],"dst_MAC": y[2]}
+            			if FlowCol.count_documents(dict) > 0:
+            				print(FlowCol.find_one(dict)["counter"])
+            				ndict={"$set": {"ID":ID, "Layer":Layer, "Hash": y[0], "src_MAC": y[1],"dst_MAC": y[2], "counter":FlowCol.find_one(dict)["counter"] + float(y[3])}}
+            				FlowCol.update_one(dict, ndict)
+            			else:
+            				dict={"ID":ID, "Layer":Layer, "Hash": y[0], "src_MAC": y[1],"dst_MAC": y[2], "counter":float(y[3])}
+            				FlowCol.insert_one(dict)
+            		
+            		if json_data["Layer"] == 3:
+            			dict={"ID":ID, "Layer":Layer, "Hash": y[0], "src_IP": y[1],"dst_IP": y[2]}
+            			if FlowCol.count_documents(dict) > 0:
+            				print(FlowCol.find_one(dict)["counter"])
+            				ndict={"$set": {"ID":ID, "Layer":json_data["Layer"], "Hash": y[0], "src_IP": y[1],"dst_IP": y[2], "counter":FlowCol.find_one(dict)["counter"] + float(y[3])}}
+            				FlowCol.update_one(dict, ndict)
+            			else:
+            				dict={"ID":ID, "Layer":Layer, "Hash": y[0], "src_IP": y[1],"dst_IP": y[2], "counter":float(y[3])}
+            				FlowCol.insert_one(dict)
+            		
+            		if json_data["Layer"] == 4:
+            			dict={"ID":ID, "Layer":Layer, "Hash": y[0], "src_IP": y[1],"dst_IP": y[2], "Porto":y[3], "src_Port":y[4], "dst_Port":y[5]}
+            			if FlowCol.count_documents(dict) > 0:
+            				print(FlowCol.find_one(dict)["counter"])
+            				ndict={"$set": {"ID":ID, "Layer":json_data["Layer"], "Hash": y[0], "src_IP": y[1],"dst_IP": y[2], "Porto":y[3], "src_Port":y[4], "dst_Port":y[5], "counter":FlowCol.find_one(dict)["counter"] + float(y[6])}}
+            				FlowCol.update_one(dict, ndict)
+            			else:
+            				dict={"ID":ID, "Layer":Layer, "Hash": y[0], "src_IP": y[1],"dst_IP": y[2], "Porto":y[3], "src_Port":y[4], "dst_Port":y[5], "counter":float(y[6])}
+            				FlowCol.insert_one(dict)
+            			
+            		#for z in FlowCol.find():
+            			#print(z)
+  
+    # connection closed 
+    c.close() 
+  
+  
+def Main(): 
+    host = "" 
+  
+    # reverse a port on your computer 
+    # in our case it is 12345 but it 
+    # can be anything 
+    port = 8080
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.bind((host, port)) 
+    print("socket binded to port", port) 
+  
+    # put the socket into listening mode 
+    s.listen(5) 
+    print("socket is listening") 
+  
+    # a forever loop until client wants to exit 
+    while True: 
+  
+        # establish connection with client 
+        c, addr = s.accept() 
+  
+        # lock acquired by client 
+        print_lock.acquire() 
+        print('Connected to :', addr[0], ':', addr[1]) 
+  
+        # Start a new thread and return its identifier 
+        start_new_thread(threaded, (c,)) 
+    s.close() 
+  
+  
+if __name__ == '__main__': 
+    Main() 
+
